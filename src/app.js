0ae9079ce7b0d635d768c262ee20a95976f04dd8
kayleynql <qiulin.ng@gmail.com>
2023-06-01 14:37:03 +0100

    Updated time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
+function formatDate(timestamp){
+    let date = new Date (timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`};
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`};
+    let days =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    let day = days[date.getDay()]; 
+
+
+    return `${day} ${hours}: ${minutes}`
+}
 function displayTemperature(response) {
     console.log(response.data);
     let temperatureElement= document.querySelector("#temperature");
@@ -10,7 +24,8 @@ function displayTemperature(response) {
     humidityElement.innerHTML= response.data.main.humidity;
     let windElement=document.querySelector("#wind");
     windElement.innerHTML= Math.round(response.data.wind.speed);
-    
+    let dateElement=document.querySelector("#date");
+    dateElement.innerHTML= formatDate(response.data.dt*1000);
 }
 let apiKey = "ed238469f9b5e9d801834270e65449bc";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
