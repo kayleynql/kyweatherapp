dedc1e8033d10b9fe0f65427d3a7e427d31aed43
kayleynql <qiulin.ng@gmail.com>
2023-06-01 15:39:22 +0100

    created search bar

--- src/app.js
+++ src/app.js
@@ -28,7 +28,19 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt",`https://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`);
 
 }
-let apiKey = "ed238469f9b5e9d801834270e65449bc";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Singapore&appid=${apiKey}&units=metric`;
+function search (city){
+    let apiKey = "ed238469f9b5e9d801834270e65449bc";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit",handleSubmit);
